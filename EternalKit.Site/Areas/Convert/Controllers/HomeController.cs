bc9aeb48ef81fc2917aabb51763c89c85c032aef
Navid Aghajari <navid.ajr28@gmail.com>
2022-04-17 00:57:31 +0430

    Added Convert Aread And Home, Length Controllers to it

---
+++ EternalKit.Site/Areas/Convert/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternalKit.Site.Areas.Convert.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EternalKit.Site/Areas/Convert/Controllers/LengthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternalKit.Site.Areas.Convert.Controllers
+{
+    public class LengthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
