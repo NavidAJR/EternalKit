d41dfde26e80606550db8b6ad3d3adf549b8315e
Navid Aghajari <navid.ajr28@gmail.com>
2022-05-09 15:01:18 +0430

    Moved Seprate methode to another class. Make small changes in TextGenerator Class

---
+++ EternalKit.Application/Utilities/DigitSeparator.cs
@@ -0,0 +1,40 @@
+﻿namespace EternalKit.Application.Utilities
+{
+    public static class DigitSeparator
+    {
+        public static string Separate(this decimal input, int digitsAfterDecimalPoint)
+        {
+            //To check if the number has decimal point.
+            if (!input.ToString().Contains('.'))
+                return input.ToString("##,###");
+
+
+            //Split the number in two part: numbers before decimal point and numbers after decimal point.
+            var inputString = input.ToString();
+            var beforeDecimalPoint = inputString.Split('.')[0];
+            var afterDecimalPoint = inputString.Split('.')[1];
+
+
+            //Add , character after every 3 number.
+            var mainPart = "";
+            var separatorIndex = 0;
+            foreach (var digit in beforeDecimalPoint.ToCharArray())
+            {
+                if (separatorIndex == 3)
+                {
+                    mainPart += ",";
+                    separatorIndex = 0;
+                }
+
+                mainPart += digit;
+                separatorIndex++;
+            }
+
+
+            if (afterDecimalPoint.Length >= digitsAfterDecimalPoint)
+                return mainPart + "." + afterDecimalPoint.Substring(0, digitsAfterDecimalPoint);
+
+            return mainPart + "." + afterDecimalPoint.Substring(0);
+        }
+    }
+}
--- EternalKit.Application/Utilities/TextGenerator.cs
+++ EternalKit.Application/Utilities/TextGenerator.cs
@@ -15,39 +15,7 @@ namespace EternalKit.Application.Utilities
             var toUnitText = LengthConversionFactor.LengthUnits.FirstOrDefault(u => u.Value == toUnitValue).Key;
 
             return
-                @$"<p>مقدار <span class=""text-primary"">{fromValue} {fromUnitText}</span> برابر با <span class=""text-success"">{result.DigitSeparatorWithDecimalPoint(digitsAfterDecimalPoint)} {toUnitText}</span> است</p>";
-        }
-
-
-        public static string DigitSeparatorWithDecimalPoint(this decimal input, int digitsAfterDecimalPoint)
-        {
-            if (!input.ToString().Contains('.'))
-                return input.ToString("##,###");
-
-
-            var inputString = input.ToString();
-            var beforeDecimalPoint = inputString.Split('.')[0];
-            var afterDecimalPoint = inputString.Split('.')[1];
-
-
-            var separatedDigits = "";
-            var separatorIndex = 0;
-            foreach (var digit in beforeDecimalPoint.ToCharArray())
-            {
-                if (separatorIndex == 3)
-                {
-                    separatedDigits += ",";
-                    separatorIndex = 0;
-                }
-                separatedDigits += digit;
-                separatorIndex++;
-            }
-
-
-            if (afterDecimalPoint.Length >= digitsAfterDecimalPoint)
-                return separatedDigits + "." + afterDecimalPoint.Substring(0, digitsAfterDecimalPoint);
-
-            return separatedDigits + "." + afterDecimalPoint.Substring(0);
+                @$"<p>مقدار <span class=""text-primary"">{fromValue} {fromUnitText}</span> برابر با <span class=""text-success"">{result.Separate(digitsAfterDecimalPoint)} {toUnitText}</span> است</p>";
         }
     }
 }
