2cbb7b42b83204075c1e3bc9442681cde4481837
Navid Aghajari <navid.ajr28@gmail.com>
2022-05-09 19:24:08 +0430

    Added home controller to Text Area and created a view for its Index action.

---
+++ EternalKit.Site/Areas/Text/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternalKit.Site.Areas.Text.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
